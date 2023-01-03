ddc952feece1eb2827aa7e933c03e9ee87b0df04
Simran Tamang <simrant009@gmail.com>
2023-01-03 18:34:33 +0545

    Self practice

---
+++ self/first/README.md
@@ -0,0 +1,12 @@
+- widgets starts with capital letters
+
+##### AppBar
+
+- appBar is property of widget Scaffold and value of appBar is widget i.e AppBar
+
+##### properties:
+
+- title: Text("fgsdf); Text is another widget
+- centerTitle: true; To keep the title at center
+
+> There should be property inside the widget . when nested something directly inside the widget then we use child Property
---
+++ self/first/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My first App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello Everyone"),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Text("Click"),
+      // ),
+    )),
+  );
+}
