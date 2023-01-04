fedd8cd97d7f27c75046f40e5b2e5b65ef2ddb0f
Simran Tamang <simrant009@gmail.com>
2023-01-04 21:30:24 +0545

    day3 project2

--- Assesement/a_day_03/lib/main.dart
+++ Assesement/a_day_03/lib/main.dart
@@ -22,135 +22,146 @@ class day03 extends StatelessWidget {
           centerTitle: true,
         ),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image(
-                image: NetworkImage(
-                  "https://res.cloudinary.com/uktv/image/upload/v1375370891/err4wnmueojcwzrrsucu.jpg",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: Text(
-                  "Settings",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image(
+                  image: NetworkImage(
+                    "https://res.cloudinary.com/uktv/image/upload/v1375370891/err4wnmueojcwzrrsucu.jpg",
                   ),
                 ),
-              ),
-              Container(
-                // height: 40,
-                padding: EdgeInsets.all(15),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                child: Row(
-                  // mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Text(
+                    "Settings",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  // height: 40,
+                  padding: EdgeInsets.all(15),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.person_outline,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(
+                              "All Users",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "100+",
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  // height: 40,
+                  padding: EdgeInsets.all(15),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.person_outline,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Employee",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            Container(
+                              width: 282,
+                              child: Text(
+                                  "Supoorting our employee Thank you everyo dfjhsdhjfb jhjk"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.star_border,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Favourite",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text("Supporting text"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(children: [
                     Icon(
-                      Icons.supervised_user_circle_rounded,
+                      Icons.email_outlined,
                     ),
                     Padding(
                       padding: const EdgeInsets.only(left: 10),
                       child: Text(
-                        "All Users",
+                        "Emails",
                         style: TextStyle(
                           fontSize: 18,
                         ),
                       ),
-                    ),
-                    Text(
-                      "100+",
-                      style: TextStyle(
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
+                    )
+                  ]),
                 ),
-              ),
-              Container(
-                // height: 40,
-                padding: EdgeInsets.all(15),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                child: Row(
-                  // mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.supervised_user_circle_rounded,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Employee",
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          Text("Supoorting our employee Thank you everyo")
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(15),
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.star_border,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Favourite",
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          Text("Supporting text"),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(15),
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Row(children: [
-                  Icon(
-                    Icons.email_outlined,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Emails",
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                  )
-                ]),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
