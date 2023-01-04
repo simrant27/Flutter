43352b99458b3ea89aaf6730de393989b3ebf2b7
Simran Tamang <simrant009@gmail.com>
2023-01-04 11:41:46 +0545

    Day2

---
+++ README.md
@@ -0,0 +1,5 @@
+#### main axis alignment in row
+
+- spaceevenly
+
+#### mainAxixSize:mainAxisSIze.min: thikka ko height lini
---
+++ day_02/lib/assesmentreview.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Review());
+}
+
+class Review extends StatelessWidget {
+  const Review({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("fdgf")),
+      ),
+    );
+  }
+}
