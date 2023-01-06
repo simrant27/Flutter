247ad88e98858b1e3fc5bd796c39d47175d1b02a
Simran Tamang <simrant009@gmail.com>
2023-01-06 16:31:23 +0545

    update

--- Assesement/a_day_03/lib/card.dart
+++ Assesement/a_day_03/lib/card.dart
@@ -1,154 +1,152 @@
+import 'package:a_day_03/main.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Card());
-}
-
-class Card extends StatelessWidget {
-  const Card({super.key});
+class CustomCard extends StatelessWidget {
+  const CustomCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(color: Colors.white),
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                    // height: 100,
-                    child: Row(
-                      // mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Header",
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(color: Colors.white),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  // height: 100,
+                  child: Row(
+                    // mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Header",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Text(
+                              "Subhead",
                               style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8),
-                              child: Text(
-                                "Subhead",
-                                style: TextStyle(
-                                  fontSize: 22,
-                                ),
-                              ),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.more_vert),
+                    ],
+                  ),
+                ),
+                Image(
+                  image: NetworkImage(
+                      "https://cdn.shopify.com/s/files/1/0277/5737/9681/products/Samolevsky.com-96Geometricshapesandlogomarkscollection08_740x.jpg?v=1599837765"),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Title",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 30),
+                        child: Text(
+                          "Subhead",
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ),
+                      Text(
+                        "Hello everuh gidbusg dghdb dhbd idjksn dhbdb jdbfs kjdbsd dbfjs djbffg df",
+                        style: TextStyle(
+                          fontSize: 22,
+                          wordSpacing: 5,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      OutlinedButton(
+                        onPressed: () {
+                          print("enabled");
+                        },
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.resolveWith(
+                            (states) => RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
                             ),
-                          ],
+                          ),
+                          side: MaterialStateProperty.all(
+                            BorderSide(
+                              color: Colors.black,
+                              width: 2,
+                            ),
+                          ),
+                          padding: MaterialStateProperty.resolveWith(
+                            (states) => EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 16),
+                          ),
                         ),
-                        Icon(Icons.more_vert),
-                      ],
-                    ),
-                  ),
-                  Image(
-                    image: NetworkImage(
-                        "https://cdn.shopify.com/s/files/1/0277/5737/9681/products/Samolevsky.com-96Geometricshapesandlogomarkscollection08_740x.jpg?v=1599837765"),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Title",
+                        child: Text(
+                          "Enabled",
                           style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w500,
+                            fontSize: 24,
+                            color: Color(0xFFB6750a4),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10, bottom: 30),
-                          child: Text(
-                            "Subhead",
-                            style: TextStyle(fontSize: 24),
-                          ),
-                        ),
-                        Text(
-                          "Hello everuh gidbusg dghdb dhbd idjksn dhbdb jdbfs kjdbsd dbfjs djbffg df",
-                          style: TextStyle(
-                            fontSize: 22,
-                            wordSpacing: 5,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        OutlinedButton(
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 20),
+                        child: ElevatedButton(
                           onPressed: () {
-                            print("enabled");
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => day03(),
+                            ));
                           },
                           style: ButtonStyle(
-                            shape: MaterialStateProperty.resolveWith(
-                              (states) => RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
+                              backgroundColor:
+                                  MaterialStateProperty.resolveWith(
+                                (states) => Color(0xFFB6750a4),
                               ),
-                            ),
-                            side: MaterialStateProperty.all(
-                              BorderSide(
-                                color: Colors.black,
-                                width: 2,
+                              shape: MaterialStateProperty.resolveWith(
+                                (states) => RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
                               ),
-                            ),
-                            padding: MaterialStateProperty.resolveWith(
-                              (states) => EdgeInsets.symmetric(
-                                  horizontal: 24, vertical: 16),
-                            ),
-                          ),
+                              padding: MaterialStateProperty.resolveWith(
+                                (states) => EdgeInsets.symmetric(
+                                    horizontal: 22, vertical: 15),
+                              )),
                           child: Text(
                             "Enabled",
                             style: TextStyle(
-                              fontSize: 24,
-                              color: Color(0xFFB6750a4),
+                              fontSize: 25,
                             ),
                           ),
                         ),
-                        Container(
-                          margin: EdgeInsets.only(left: 20),
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.resolveWith(
-                                  (states) => Color(0xFFB6750a4),
-                                ),
-                                shape: MaterialStateProperty.resolveWith(
-                                  (states) => RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
-                                ),
-                                padding: MaterialStateProperty.resolveWith(
-                                  (states) => EdgeInsets.symmetric(
-                                      horizontal: 22, vertical: 15),
-                                )),
-                            child: Text(
-                              "Enabled",
-                              style: TextStyle(
-                                fontSize: 25,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
--- Assesement/a_day_03/lib/main.dart
+++ Assesement/a_day_03/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:a_day_03/card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(day03());
+  runApp(MaterialApp(home: CustomCard()));
 }
 
 class day03 extends StatelessWidget {
@@ -9,161 +10,158 @@ class day03 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.purple[50],
+      appBar: AppBar(
         backgroundColor: Colors.purple[50],
-        appBar: AppBar(
-          backgroundColor: Colors.purple[50],
-          title: Text(
-            "Flutter Bootcamp",
-            style: TextStyle(color: Colors.black),
-          ),
-          centerTitle: true,
+        title: Text(
+          "Flutter Bootcamp",
+          style: TextStyle(color: Colors.black),
         ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image(
-                  image: NetworkImage(
-                    "https://res.cloudinary.com/uktv/image/upload/v1375370891/err4wnmueojcwzrrsucu.jpg",
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image(
+                image: NetworkImage(
+                  "https://res.cloudinary.com/uktv/image/upload/v1375370891/err4wnmueojcwzrrsucu.jpg",
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  "Settings",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    "Settings",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Container(
-                  // height: 40,
-                  padding: EdgeInsets.all(15),
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: Row(
-                    // mainAxisAlignment: MainAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.person_outline,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10),
-                            child: Text(
-                              "All Users",
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
+              ),
+              Container(
+                // height: 40,
+                padding: EdgeInsets.all(15),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.person_outline,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                            "All Users",
+                            style: TextStyle(
+                              fontSize: 18,
                             ),
                           ),
-                        ],
-                      ),
-                      Text(
-                        "100+",
-                        style: TextStyle(
-                          fontSize: 12,
                         ),
+                      ],
+                    ),
+                    Text(
+                      "100+",
+                      style: TextStyle(
+                        fontSize: 12,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Container(
-                  // height: 40,
-                  padding: EdgeInsets.all(15),
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: Row(
-                    // mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Icon(
-                        Icons.person_outline,
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Employee",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                ),
-                              ),
-                              Text(
-                                "Supoorting our employee Thank you everyo dfjhsdhjfb jhjk",
-                                // maxLines: 5,
-                                // softWrap: true,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(15),
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.star_border,
-                      ),
-                      Padding(
+              ),
+              Container(
+                // height: 40,
+                padding: EdgeInsets.all(15),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.person_outline,
+                    ),
+                    Expanded(
+                      child: Padding(
                         padding: const EdgeInsets.only(left: 10),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              "Favourite",
+                              "Employee",
                               style: TextStyle(
                                 fontSize: 18,
                               ),
                             ),
-                            Text("Supporting text"),
+                            Text(
+                              "Supoorting our employee Thank you everyo dfjhsdhjfb jhjk",
+                              // maxLines: 5,
+                              // softWrap: true,
+                            ),
                           ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Container(
-                  padding: EdgeInsets.all(15),
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Row(children: [
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
                     Icon(
-                      Icons.email_outlined,
+                      Icons.star_border,
                     ),
                     Padding(
                       padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        "Emails",
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Favourite",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text("Supporting text"),
+                        ],
                       ),
-                    )
-                  ]),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Row(children: [
+                  Icon(
+                    Icons.email_outlined,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Emails",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            ],
           ),
         ),
       ),
