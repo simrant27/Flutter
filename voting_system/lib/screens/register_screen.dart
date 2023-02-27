245321037b0a67b664de4b7570188f56fb3393dc
Simran Tamang <simrant009@gmail.com>
2023-02-27 10:51:20 +0545

    login and register done

--- voting_system/lib/screens/register_screen.dart
+++ voting_system/lib/screens/register_screen.dart
@@ -113,7 +113,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               .showSnackBar(SnackBar(content: Text(decoded['message'])));
         }
       }
-      clearData();
+      // clearData();
     }
   }
 
